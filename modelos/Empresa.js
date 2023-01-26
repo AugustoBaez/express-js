793d27f862f3a192a4a0954394a033673135d71b
asbae <asbaez@outlook.com>
2023-01-26 01:47:55 -0300

    change

--- modelos/Empresa.js
+++
@@ -1,8 +0,0 @@
-let { faker } = require("@faker-js/faker");
-module.exports = class Empresa {
-    constructor() {
-        this.name = faker.company.name()
-        this.street = faker.address.name()
-        this.city = faker.address.city()
-    }
-}
--- modelos/Usuario.js
+++
@@ -1,9 +0,0 @@
-let { faker } = require("@faker-js/faker");
-
-module.exports = class Usuario {
-    constructor() {
-        this.name = faker.name.firstName();
-        this.lastName = faker.name.lastName();
-        this.email = faker.internet.email();
-    }
-}
